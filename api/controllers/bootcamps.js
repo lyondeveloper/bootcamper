6acf66db835b4355853da446a8c233bae0978eaf
Jesus Rincon <jrincon_externo@rayensalud.com>
2019-11-08 16:16:29 -0300

    created basic CRUD

--- api/controllers/bootcamps.js
+++ api/controllers/bootcamps.js
@@ -1,34 +1,83 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc   Get All bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.json({ msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.json({ success: true, count: bootcamps.length, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getSingleBootcamp = (req, res, next) => {
-  res.json({ msg: `Show bootcamp ${req.params.id}` });
+exports.getSingleBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Create bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-  res.json({ msg: 'create bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Edit bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.editBootcamp = (req, res, next) => {
-  res.json({ msg: 'edit bootcamp ' });
+exports.editBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
-  res.json({ msg: 'delete bootcamp' });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
+require('colors');
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const connectDb = require('./config/db');
+
 const app = express();
 
-const dotenv = require('dotenv');
-
-// Middlewares
-const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+// Body parse
+app.use(express.json());
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Dev loggin middleware
 if (process.env.NODE_ENV === 'development') {
@@ -20,8 +23,20 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+// Connect to db
+connectDb();
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode in port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode in port ${PORT}`.yellow.bold
+  );
+});
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  // Close server and exit process
+  server.close(() => process.exit(1));
 });
