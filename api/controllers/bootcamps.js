ceb850cd2c04a6c95fa1fe6897cdbe749c83ab02
Jesus Rincon <jrincon_externo@rayensalud.com>
2019-11-14 16:00:39 -0300

    working on store to fetch data with redux and redux saga

--- api/controllers/bootcamps.js
+++ api/controllers/bootcamps.js
@@ -10,7 +10,7 @@ exports.getBootcamps = async (req, res, next) => {
 
     res.json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -29,8 +29,7 @@ exports.getSingleBootcamp = async (req, res, next) => {
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    // res.status(400).json({ success: false });
-    next(new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404));
+    next(err);
   }
 };
 
@@ -45,7 +44,7 @@ exports.createBootcamp = async (req, res, next) => {
       data: bootcamp
     });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -60,12 +59,14 @@ exports.editBootcamp = async (req, res, next) => {
     });
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -77,11 +78,13 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ success: true, data: {} });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 // components
 import Header from './components/commons/navbar/navbar.component';
 import Homepage from './components/pages/homepage/homepage.component';
+import Auth from './components/pages/auth/auth.component';
 
 import GlobalStyles from './global.styles';
 
-function App() {
+const App = () => {
   return (
     <Fragment>
       <Router>
         <Header />
         <GlobalStyles />
+        <ToastContainer />
         <Switch>
           <Route exact path='/' component={Homepage} />
+          <Route path='/auth' component={Auth} />
         </Switch>
       </Router>
     </Fragment>
   );
-}
+};
 
 export default App;
