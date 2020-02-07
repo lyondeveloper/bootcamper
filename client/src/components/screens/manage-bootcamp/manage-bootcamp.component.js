e22275c305bfdb1bf05c3ecd9c45c46619deb53f
Jesus Rincon <jesus.rincon@Jesuss-MacBook-Pro.local>
2020-02-07 19:52:07 -0300

    changed

--- client/src/components/screens/manage-bootcamp/manage-bootcamp.component.js
+++ client/src/components/screens/manage-bootcamp/manage-bootcamp.component.js
@@ -72,7 +72,7 @@ const ManageBootcamp = () => {
               </Button>
             </Form>
             <div className="mb-2">
-              <Link to={`/edit-bootcamp/${bootcamp.id}`}>
+              <Link to={"/"}>
                 <Button color="primary" className="btn-block">
                   {" "}
                   Edit Bootcamp Details{" "}
