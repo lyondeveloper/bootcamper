c7aa0b727c637257febfdf72678a1afc9cbbf8b8
Jesus Rincon <jesus.rincon@Jesuss-MacBook-Pro.local>
2020-02-07 19:48:27 -0300

    changed some things

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
